80dc9edaa308dbf77b2a1a7cc980d32c58dcdc5e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-10-07 08:30:33 +0800

    hm2014811: Tune specifically for 1GB devices
    * These are more common than 2GB.
    * Will need a separate commit to tune for 2GB devices.
    
    Change-Id: I7e79e6af6df7ecd979df60e47e2994d9279f3c5f

--- device.mk
+++ device.mk
@@ -34,8 +34,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
@@ -91,9 +90,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # Ramdisk
-PRODUCT_PACKAGES += \
-    init.qcom.lowmem.sh
-
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc
